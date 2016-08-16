44c4ee791f0da74ee4d8ff1c049c97e2041b61f8
Jason Kirouac <jkirouac42@gmail.com>
2016-08-16 12:05:32 -0400

    added some new stuff

--- B4B/B4B/Controllers/HomeController.cs
+++ B4B/B4B/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using B4B.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace B4B.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         [AllowAnonymous]
         public ActionResult Index()
         {
@@ -25,6 +27,21 @@ namespace B4B.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        //Created view to allow admin to create profiles
+        //GET: Home/Admin/5
+        [Authorize]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        //POST: Home/Admin/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Admin([Bind(Include = "")] Profile Profile)
+        {
             return View();
         }
     }
