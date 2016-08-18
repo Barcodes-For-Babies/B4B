b02698b762fc3fee6b86fd741800078bb2427a52
Malyssa Winters <malyssawinters5@gmail.com>
2016-08-18 15:01:13 -0400

    added twilio class

---
+++ B4B/B4B/Models/Twilio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Twilio;
+
+namespace twilio_sms
+{
+    public class Twilio
+{
+    static void Main(string[] args)
+    {
+        var client = new TwilioRestClient(Environment.GetEnvironmentVariable("ACd48b70e60a914867cba9c9b806374d3b"),
+            Environment.GetEnvironmentVariable("d7e27b30fdf58495555421afbec490f7"));
+
+            client.SendMessage("+12164506056", "+14193667629", "Emergency button has been activated!");
+    }        
+}
+}
