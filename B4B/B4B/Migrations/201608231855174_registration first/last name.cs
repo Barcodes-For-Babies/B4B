8be075a936ef8c0634571461edae5f299c2f13ae
Malyssa Winters <malyssawinters5@gmail.com>
2016-08-23 15:53:00 -0400

    UX login/register/home

---
+++ B4B/B4B/Migrations/201608231855174_registration first/last name.cs
@@ -0,0 +1,16 @@
+namespace B4B.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class registrationfirstlastname : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
